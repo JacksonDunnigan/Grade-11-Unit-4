a30e6aa2e9a603d8b91c7400dffb0605469db6d7
s241722 <s241722@shha216m28.ocsb.local>
2018-11-13 10:49:41 -0500

    finished sum thangs

--- ICS3UC_Unit4/src/activity4/Payroll.java
+++ ICS3UC_Unit4/src/activity4/Payroll.java
@@ -1,6 +1,6 @@
 package activity4;
-
-include simpleIO.Console;
+//Syntax error: the syntax is import not include
+import simpleIO.Console;
 
 /**
  * Payroll This program will calculate the gross pay for each employee, as well
@@ -17,7 +17,8 @@ public class Payroll {
     final static int OVERTIME_HOURS = 40;
     final static double OVERTIME_RATE = 1.5;
     
-    public static void main(String args) {
+    //Syntax error: Must add [] after String
+    public static void main(String[] args) {
 
         int numberOfEmployees;
         
@@ -26,37 +27,57 @@ public class Payroll {
         int hoursWorked;
         double grossPay;
         
-        double totalPayroll;
+        //Syntax error: must give a base value if modified in a loop other method
+        double totalPayroll = 0;
         
-        numberOfEmployees = Console.readChar("How many employees do you have?");
+        //Syntax error: you have to read int not char
+        numberOfEmployees = Console.readInt("How many employees do you have?");
         
-        for (i = 0, i < numberOfEmployees, i++); {
+        // Syntax error: you must declare the counter variable with "Int" 
+        // Syntax error: you don't end a loop declaration with a semicolon
+        // Syntax error: you separate each element of the loop with a semi colon not a comma
+        
+        for (int i = 0; i < numberOfEmployees; i++) {
+        	
             employeeName = Console.readString("\nEnter employee name: ");
             hourlyWage = Console.readInt("Enter employee's hourly wage: ");
             hoursWorked = Console.readInt("Enter hours worked: ");
             
-            grossPay = getgrosspay(hoursWorked, hourlyWage);
+            //Syntax Error: Capitalization is wrong
+            //Logic error: inputs are imported in the wrong order
+            grossPay = getGrossPay(hourlyWage, hoursWorked);
             
-            totalPayroll + grossPay;
+            //Syntax Error: must use += to change a variable
+            totalPayroll += grossPay;
             
-            Console.print("\n", employeeName, " earned $", false);
+            //syntax error: too many arguments, use '+' instead of ','
+            Console.print("\n"+employeeName+" earned $", false);
             Console.print(grossPay, 2);
         }
+        
+        //Syntax error: trying to run script outside of the method
+        Console.print("\nTotal payroll $", false);
+        Console.print(totalPayroll, 2);
     }
     
-    Console.print("\nTotal payroll $", false);
-    Console.print(totalPayroll, 2);
+
     
     public static double getGrossPay(double hourlyWage, int hoursWorked) {
         double grossPay;
         
-        if (hoursWorked < OVERTIME_HOURS) 
-            extraHours = hoursWorked - OVERTIME_HOURS;
+        //Syntax error: no brackets in if statements
+        //Logic error: should check if its greater than overtime hours rather than less than overtime hours
+        if (hoursWorked > OVERTIME_HOURS) {
+        	//syntax error: must add a declaration for extraHours
+            int extraHours = hoursWorked - OVERTIME_HOURS;
             double newRate = hourlyWage * OVERTIME_RATE;
-            grossPay = OVERTIME_HOURS * hourlyWage + extraHours * OVERTIME_RATE;
-        else 
+            
+            //logic error: should multiply extra hours by new rate rather than overtime rate
+            grossPay = OVERTIME_HOURS * hourlyWage + extraHours * newRate;
+        }
+        else {    
             grossPay = hoursWorked * hourlyWage;
-        
+        }
         return grossPay;
     }
 
