4dfd3c13d2a9d4fcd78396fe8c4547f7d41fc981
s241722 <s241722@shha216m04.ocsb.local>
2018-11-20 10:54:05 -0500

    finished the time converter and time class object

--- ICS3UC_Unit4/src/activity5/Time.java
+++ ICS3UC_Unit4/src/activity5/Time.java
@@ -1,45 +1,41 @@
 package activity5;
 
-public class Time {
-	String time;
-	int hour;
-	int minute;
-	int second;
-	
-	Time(String time){
-	}
-	
-	// converts the string to integers
-	public int convertToInt() {
 
-		hour = Integer.parseInt(time.substring(0, time.indexOf(":")-1));
-		minute = Integer.parseInt(time.substring(time.indexOf(":"), time.indexOf(":",time.indexOf(":"))-1));
-		second = Integer.parseInt(time.substring(time.indexOf(":"), time.length()-1));
+public class Time {
+	//creates variables to hold the hours, minutes and seconds
+	int hours,minutes,seconds;
+	
+	Time(String time) {
 		
-		return hour;
+		// splices the input to a series of integers
+		hours = Integer.parseInt(time.substring(0, time.indexOf(':') ));
+		minutes = Integer.parseInt(time.substring(time.indexOf(':') + 1, time.lastIndexOf(':')));
+		seconds = Integer.parseInt(time.substring(time.lastIndexOf(':') + 1, time.length() ));
 	}
-	
-	
-	
+
+
 	// converts the time to 12 hour notation
-	public String convertDay() {
-		String date;
-		String ending = "AM";
-		
-		if (hour>=12) {
-			ending = "PM";
-			
-			if (hour>=13) {
-				hour -= 12;
+	public String convertTime() {
+		String output;
+		String ending = "PM";
+
+		//makes the time 12 hour notation
+		if (hours >= 12) {
+
+			if (hours >= 13) {
+				hours -= 12;
+				ending = "PM";
 			}
-			
+
 		} else {
 			ending = "AM";
 		}
+
+		// makes it a new string
+		output = ""+ hours + ':' + minutes + ':' + seconds + ' ' + ending;
 		
-		//makes it a new string
-		date = hour+':'+minute+':'+second+':'+ending;
-		return date;
+		//returns the new time created
+		return output;
 	}
 
 }
--- ICS3UC_Unit4/src/activity5/timeConverter.java
+++ ICS3UC_Unit4/src/activity5/timeConverter.java
@@ -8,10 +8,18 @@ public class timeConverter {
 		// input
 		String input = Console.readString("Enter a time in 24 hour notation: ");
 		
-
+		//creates a time object
+		Time t = new Time(input);
+//		Console.print(t.hours);
+//		Console.print(t.minutes);
+//		Console.print(t.seconds);
 		
+		//covers the integers to a date
+		String date = t.convertTime();
 		
-		// creates an abject
+		//prints the new date
+		Console.print(input + " in 12 hour notation is: "+date);
+		
 		
 	}
 }
